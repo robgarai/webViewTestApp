be569475d308d920f4eefd8691ddde40e6e05789
DESKTOP-ERIDUAN\rober <robert.garai@gmail.com>
2018-08-13 12:46:44 +0200

    first commit

---
+++ app/src/main/java/com/example/rober/webviewtestapp/activities.kt
@@ -0,0 +1,5 @@
+package com.example.rober.webviewtestapp
+
+/**
+ * Created by rober on 09.08.2018.
+ */
---
+++ app/src/main/java/com/example/rober/webviewtestapp/activities/WebViewActivity.java
@@ -0,0 +1,8 @@
+package com.example.rober.webviewtestapp.activities;
+
+/**
+ * Created by rober on 09.08.2018.
+ */
+
+public class WebViewActivity {
+}
