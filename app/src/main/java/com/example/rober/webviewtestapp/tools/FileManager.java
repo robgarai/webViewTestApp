174b7e5255a28641c32ae504aff44ddbeb91248d
DESKTOP-ERIDUAN\rober <robert.garai@gmail.com>
2018-08-17 16:25:36 +0200

    skoro funguje metoda vyries static content

--- app/src/main/java/com/example/rober/webviewtestapp/tools/FileManager.java
+++ app/src/main/java/com/example/rober/webviewtestapp/tools/FileManager.java
@@ -1,6 +1,7 @@
 package com.example.rober.webviewtestapp.tools;
 
 import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import com.example.rober.webviewtestapp.R;
@@ -15,18 +16,15 @@ import java.io.InputStreamReader;
 
 import static android.content.ContentValues.TAG;
 
-public class FileManager {
+public class FileManager extends AppCompatActivity {
 
-
-
-
-    public static String readStringFromResource(Context ctx, int resourceID) {
+    public String readStringFromResource(int resourceID) {
         StringBuilder stringBuilder = new StringBuilder();
-        String sep = System.getProperty("line.separator");
+        //String sep = System.getProperty("line.separator");
 
         try {
             //InputStream inputStream = ctx.getResources().openRawResource(R.raw.earth_shadow_base64);
-            InputStream inputStream = ctx.getResources().openRawResource(resourceID);
+            InputStream inputStream = getResources().openRawResource(resourceID);
             InputStreamReader inputStreamReader = new InputStreamReader (inputStream) ;
             BufferedReader bufferedReader = new BufferedReader(inputStreamReader, 1024*8);
 
@@ -34,7 +32,7 @@ public class FileManager {
                 String line = null;
                 while (( line = bufferedReader.readLine()) != null){
                     stringBuilder.append(line);
-                    stringBuilder.append(sep);
+                    //stringBuilder.append(sep);
                 }
             }
             finally {
