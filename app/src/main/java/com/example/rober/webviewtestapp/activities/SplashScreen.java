37b61201a99bff6ccd99cb82acfd855c1eca4d51
DESKTOP-ERIDUAN\rober <robert.garai@gmail.com>
2018-08-21 13:56:54 +0200

    SPLASH FUNGUJE chyba post request a web view

--- app/src/main/java/com/example/rober/webviewtestapp/activities/SplashScreen.java
+++ app/src/main/java/com/example/rober/webviewtestapp/activities/SplashScreen.java
@@ -1,24 +1,53 @@
 package com.example.rober.webviewtestapp.activities;
 
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 import com.example.rober.webviewtestapp.R;
 
 
 public class SplashScreen extends AppCompatActivity {
 
+    private static final int SPLASH_DELAY = 1000;
+
+    private final Handler mHandler   = new Handler();
+    private final Launcher mLauncher = new Launcher();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        TestFairy.begin(this, "14f9d2f78928cf7b85e08b0c5a3d8467b27cd3f5"); // e.g "0000111122223333444455566667777788889999";
-        //setContentView(R.layout.activity_splash);
-        Log.i("MyMessageOnCreate", "now the app started and splash screen shows itself for 4 seconds");
-        startActivity(new Intent(SplashScreen.this, LoginActivity.class));
-        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-        finish();
+        setContentView(R.layout.activity_splash);
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mHandler.postDelayed(mLauncher, SPLASH_DELAY);
+    }
+
+    @Override
+    protected void onStop() {
+        mHandler.removeCallbacks(mLauncher);
+        super.onStop();
+    }
+
+    private void launch() {
+        if (!isFinishing()) {
+            startActivity(new Intent(SplashScreen.this, LoginActivity.class));
+            overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+            finish();
+        }
+    }
+
+    private class Launcher implements Runnable {
+        @Override
+        public void run() {
+            launch();
+        }
     }
 }
+
+
